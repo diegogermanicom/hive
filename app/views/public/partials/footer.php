8be24228fc13f276d7e520a033297effcd287bc2
Hive <diegogermanicom@gmail.com>
2025-07-31 10:54:14 -0400

    Improvements 07/2025

--- app/views/public/partials/footer.php
+++ app/views/public/partials/footer.php
@@ -19,5 +19,5 @@
         </div>
         <?php } ?>
         <div class="text-center font-14">Published under <a href="https://opensource.org/licenses/MIT" target="_blank">MIT</a> license.</div>
-        <div class="text-center font-14 pt-5">Copyright © <?= date('Y'); ?> <b class="core-color">Hive</b> Framework - <a href="<?= PUBLIC_ROUTE; ?>/privacy-policy">Privacy policy</a> - <a href="<?= PUBLIC_ROUTE; ?>/cookie-policy">Cookie policy</a></div>
+        <div class="text-center font-14 pt-5">Copyright © <?= date('Y'); ?> <b class="core-color">Hive</b> Framework - <a href="<?= Utils::getRoute('privacy-policy'); ?>">Privacy policy</a> - <a href="<?= Utils::getRoute('cookie-policy'); ?>">Cookie policy</a></div>
     </footer>
